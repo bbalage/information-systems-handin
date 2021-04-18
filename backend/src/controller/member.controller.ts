6c812ee04ead623ec07e33f46bb249028ad82f21
bbalage <bbalage0@gmail.com>
2021-04-18 13:03:09 +0200

    Get methods implemented: findById, findByIdCardNumber, findByName.
    All get methods are now linked to one single get function.

--- backend/src/controller/member.controller.ts
+++ backend/src/controller/member.controller.ts
@@ -23,7 +23,33 @@ export class MemberController extends Controller {
         }
     }
 
-    getAll = async (req, res) => {
+    get = async (req, res) => {
+        /*If there is an id in the query, then find by id.*/
+        const id = req.query.id;
+        if (id) {
+            this.getById(req, res);
+            return;
+        }
+        
+        /*If there is an idCardNumber in the query, then find by idCardNumber.*/
+        const idCardNumber = req.query.idCardNumber;
+        if (idCardNumber) {
+            this.getByIdCardNumber(req, res);
+            return;
+        }
+
+        /*If there is a name in the query, then find by name.*/
+        const name = req.query.name;
+        if (name) {
+            this.getByName(req, res);
+            return;
+        }
+
+        /*If there is no parameter in the query, then find all.*/
+        this.getAll(req, res);
+    }
+
+    private getAll = async (req, res) => {
         try {
             const allMembers = await this.repository.find();
             res.json({ success: true, data: allMembers });
@@ -34,13 +60,15 @@ export class MemberController extends Controller {
         }
     }
 
-    getById = async (req, res) => {
-        const id = req.query.id;
-        if (!id) {
-            this.handleError(res, 400, "No id given.");
-        }
+    private getById = async (req, res) => {
         try {
+            const id = req.query.id;
             const member = await this.repository.findByIds(id);
+            console.log(member);
+            if (!member || member.length == 0) {
+                this.handleError(res, 404, 'No member found with given id.');
+                return;
+            }
             res.json({ success: true, data: member });
         }
         catch (err) {
@@ -49,14 +77,44 @@ export class MemberController extends Controller {
         }
     }
 
-    getByIdCardNumber = async (req, res) => {
-        //TODO
-        this.handleError(res, 500, "Method not yet implemented");
+    private getByIdCardNumber = async (req, res) => {
+        try {
+            const idCardNumber = req.query.idCardNumber;
+            const members = await this.repository
+                .createQueryBuilder('member')
+                .where("member.idCardNumber LIKE CONCAT('%', :idCardNumber, '%')",
+                    {idCardNumber: idCardNumber})
+                .getMany();
+            if (!members || members.length == 0) {
+                this.handleError(res, 404, 'No member with that Id Card Number found.');
+                return;
+            }
+            res.json({ success: true, data: members });
+        }
+        catch (err) {
+            console.log("Could not find by Id Card Number.");
+            this.handleError(res);
+        }
     }
 
-    getByName = async (req, res) => {
-        //TODO
-        this.handleError(res, 500, "Method not yet implemented");
+    private getByName = async (req, res) => {
+        try{
+        const name = req.query.name;
+            const members = await this.repository
+                .createQueryBuilder('member')
+                .where("member.name LIKE CONCAT('%', :name, '%')",
+                    {name: name})
+                .getMany();
+            if (!members || members.length == 0) {
+                this.handleError(res, 404, 'No member with that kind of name found.');
+                return;
+            }
+            res.json({ success: true, data: members });
+        }
+        catch (err) {
+            console.log("Could not find by name.");
+            this.handleError(res);
+        }
     }
 
     delete = async (req, res) => {
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,10 +6,10 @@ export function getRouter(): Router {
 
     const memberController = new MemberController();
 
-    router.get('/members', memberController.getAll);
-    router.get('/members/id', memberController.getById);
-    router.get('/members/id-card-number', memberController.getByIdCardNumber);
-    router.get('/members/name', memberController.getByName);
+    router.get('/members', memberController.get);
+    //router.get('/members/id', memberController.getById);
+    //router.get('/members/id-card-number', memberController.getByIdCardNumber);
+    //router.get('/members/name', memberController.getByName);
     router.post('/members/create', memberController.create);
 
 
